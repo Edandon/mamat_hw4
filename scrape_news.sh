02761d6007ea0071abe8a0992c19d5665916c323
Mamat <Edandon@gmail.com>
2020-12-09 18:29:30 +0200

    Edan 091220 1830

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 wget https://www.ynetnews.com/category/3082
-grep -o 'https://www.ynetnews.com/article/[A-Za-z0-9]\+' 3082 | uniq > articles
+grep -o 'https://www.ynetnews.com/article/[A-Za-z0-9]\{9\}\b' 3082 | sort | uniq > articles
 num_of_lines=$(wc -l < articles)
 
-echo $num_of_lines
+echo $num_of_lines >> results.csv
 cat articles | while read link_line;
 	
 	do wget $link_line;
+	filename=$(basename"$link_line");
 	(( netanyahu_sum=$(grep -o Netanyahu `echo $link_line | cut -d / -f 5` | wc -l) ));
 	(( gantz_sum=$(grep -o Gantz `echo $link_line | cut -d / -f 5` | wc -l) ));
 	
-	echo $link_line, Netanyahu, $netanyahu_sum, Gantz, $gantz_sum >> results.csv
+	if [[ gantz_sum -eq 0 && netanyahu_sum -eq 0 ]]
+	then
+		echo $link_line, - >> results.csv
+	else
+		echo $link_line, Netanyahu, $netanyahu_sum, Gantz, $gantz_sum >> results.csv
+	fi
 
 done
